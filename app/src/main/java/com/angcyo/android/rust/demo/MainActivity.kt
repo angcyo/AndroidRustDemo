0b270b19f48496d8add3084e1ddd245921a08222
angcyo <angcyo@126.com>
2023-05-17 17:51:10 +0800

    init

---
+++ app/src/main/java/com/angcyo/android/rust/demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.angcyo.android.rust.demo
+
+import android.os.Bundle
+import androidx.core.app.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
